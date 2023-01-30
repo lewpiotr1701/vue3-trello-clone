2da2f82e45930a1c1a45d1d9b756c01e596199da
Piotr <lewczykpiotr@gmail.com>
2023-01-30 15:17:18 +0100

    add default-board config and install uuid

---
+++ src/services/default-board.js
@@ -0,0 +1,52 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export default {
+  name: 'workshop',
+  columns: [
+    {
+      name: 'todo',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuidv4(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'second task',
+          id: uuidv4(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'and thrid',
+          id: uuidv4(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'in-progress',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuidv4(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'done',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuidv4(),
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+}
