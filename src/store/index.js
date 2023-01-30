8b9f4f9a1e8e86293fa74a87caa30e8bc59e93ff
Piotr <lewczykpiotr@gmail.com>
2023-01-30 16:00:44 +0100

    config vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,20 @@
 import { createStore } from 'vuex'
+import defaultBoard from '@/services/default-board.js'
 
-export default {
+export default createStore({
+  state: {
+    board: defaultBoard
+  },
+  getters: {
 
-}
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+
+  }
+})
