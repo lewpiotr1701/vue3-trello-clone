565eddb4a585778f211f5e89bfa5e3193a57aceb
Piotr <lewczykpiotr@gmail.com>
2023-01-30 17:30:35 +0100

    update Board component

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="board">
-
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="(column, columnKey) in board.columns" :key="columnKey">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task" v-for="(task, taskKey) in column.tasks" :key="taskKey">
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  computed: mapState(['board'])
 }
 </script>
 
 <style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-gray-900 no-underline;
+}
+
+.column {
+  @apply bg-gray-100 p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
 .board {
   @apply p-4 bg-teal-500 h-full overflow-auto;
+}
+
+.task-bg {
+  @apply top-0 absolute;
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
